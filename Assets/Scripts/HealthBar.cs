b076b7d45984f9a50fd288edf0c653d581f7014f
Hansatang <portokarlos@gmail.com>
2023-02-21 17:44:22 +0100

    Initial commit

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+
+    public Image fill;
+    // Start is called before the first frame update
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.value / slider.maxValue);
+    }
+    
+    public void SetMaxHealth(int MaxHealth)
+    {
+        slider.maxValue = MaxHealth;
+        fill.color = gradient.Evaluate(1f);
+    }
+}
---
+++ Assets/Scripts/Narration.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Narration : MonoBehaviour, IObserver
+{
+    [SerializeField] private Subject _playerSubject;
+    public void OnNotify(string data)
+    {
+        Debug.Log("The "+ data +" has started");
+    }
+
+    private void OnEnable()
+    {
+        _playerSubject.AddObserver(this);
+    }
+        
+    private void OnDisable()
+    {
+        _playerSubject.RemoveObserver(this);
+    }
+}

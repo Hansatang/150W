6433b2dbea94499905852d9d4e5191fcfcff19c0
Hansatang <portokarlos@gmail.com>
2023-04-03 12:53:55 +0200

    Background FIx
    Im sorry

--- Assets/Scripts/UI/BackgroundHandler.cs
+++ Assets/Scripts/UI/BackgroundHandler.cs
@@ -5,20 +5,37 @@ public class BackgroundHandler : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject space;
 
+    // The radius of a possible camera view
+    private float _spaceCircleRadius;
+
     // Original background object dimensions
     private float _backgroundOriginalSizeX;
     private float _backgroundOriginalSizeY;
-
+    
     private float _cameraOffsetX;
     private float _cameraOffsetY;
 
     void Start()
     {
+
+
         // Original Background Sizes
         SpriteRenderer sr = space.GetComponent<SpriteRenderer>();
         var originalSize = sr.size;
         _backgroundOriginalSizeX = originalSize.x;
         _backgroundOriginalSizeY = originalSize.y;
+
+        // Camera height equal to the orthographic size
+        float orthographicSize = mainCamera.orthographicSize;
+        // Camera width equals to orthographic size multiplied by aspect ratio
+        float screenAspect = Screen.width / (float) Screen.height;
+        // The radius of the circle describing the camera
+        _spaceCircleRadius = Mathf.Sqrt(orthographicSize * screenAspect * orthographicSize * screenAspect +
+                                        orthographicSize * orthographicSize);
+
+        // The final background sprite size should allow you to move by one basic background texture size in any direction + overlap the radius of the camera also in all directions
+        sr.size = new Vector2(_spaceCircleRadius * 2 + _backgroundOriginalSizeX * 2,
+            _spaceCircleRadius * 2 + _backgroundOriginalSizeY * 2);
     }
 
     void Update()
